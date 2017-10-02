234e70723ca76acf838d2f0db762c14161202412
Elan Ruusamäe <glen@delfi.ee>
2017-10-02 13:52:36 +0300

    setup venv

--- convert.sh
+++ convert.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
-set -x
+set -xe
+
+if [ ! -d VENV ]; then
+	virtualenv -p python2.7 VENV
+	. VENV/bin/activate
+	pip install -r requirements/dist.txt
+	pip install -e .
+	chmod -R a+rX .
+fi
 
 ./tracboat.sh -vv --config-file="$@" migrate >out.log 2>error.log || tail error.log 
